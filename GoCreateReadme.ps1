cece6cfceb605fa85e7ca5eafa0725c0c6879fea
Devedse <davydavidse@gmail.com>
2022-11-02 01:17:37 +0100

    Created script to automatically update readme

---
+++ GoCreateReadme.ps1
@@ -0,0 +1,52 @@
+$readmeContent = Get-Content 'README.md'
+
+
+Function InsertToArray($Array, $insertIndex, $valueToInsert){
+    return @(
+        for ($i = 0; $i -lt $insertIndex; $i++) {
+            $Array[$i]
+        }
+        $valueToInsert
+        for ($i = $insertIndex; $i -lt $Array.Length; $i++) {
+            $Array[$i]
+        }
+    )
+}
+
+
+$highestNumber = 0
+$currow = 0
+$lastrow = 0
+$regex = '^\| (?<number>\d+) \| \!.*$'
+foreach ($line in $readmeContent) {
+    if ($line -match $regex) {
+        $digit = $Matches["number"]
+        $number = [int]$digit
+        
+        $highestNumber = $number
+        $lastrow = $currow
+    }
+    $currow++
+}
+
+
+while ($true) {
+    $highestNumber++    
+    $lastrow++
+
+    $filesZen = @(Get-ChildItem -Filter "*Zen_$highestNumber*.png")
+    $filesTM5 = @(Get-ChildItem -Filter "*TM5_$highestNumber*.png")
+
+    if ($filesZen.Length -eq 0 -and $filesTM5.Length -eq 0) {
+        break
+    }
+
+    $stringZen = $filesZen | ForEach-Object { "![]($($_.Name))"} | Join-String -Separator ' '
+    $stringTM5 = $filesTM5 | ForEach-Object { "![]($($_.Name))"} | Join-String -Separator ' '
+    $line = "| $highestNumber | $stringZen | $stringTM5 | ... | ... |"
+    Write-Host $line
+
+    $readmeContent = InsertToArray $readmeContent $lastrow $line
+}
+
+Set-Content -Path "README.md" $readmeContent
--- README.md
+++ README.md
@@ -4,3 +4,17 @@ My current progress in overclocking RAM
 | Step | Zentimings | TM5 | Description | Stable? |
 |:----:|:----------:|:---:|:-----------:|:-------:|
 | 1 | ![](Zen_1_3600error.png) | ![](TM5_1_Witherrors.png) | 3600mhz with increased primary timings | Unstable ~30 hours |
+| 2 | ![](Zen_2_noerror.png) | ![](TM5_2.png) | ... | ... |
+| 3 | ![](Zen_3.png) |  | ... | ... |
+| 4 |  | ![](TM5_4_tFAW.png) | ... | ... |
+| 5 | ![](Zen_5_TRFC.png) | ![](TM5_5.png) | ... | ... |
+| 6 | ![](Zen_6_tWTRS.png) | ![](TM5_6.png) | ... | ... |
+| 7 |  | ![](TM5_7.png) | ... | ... |
+| 8 | ![](Zen_8_SetupsenVddp.png) | ![](TM5_8_4herror.png) | ... | ... |
+| 9 | ![](Zen_9_Setups57_3Errors.png) | ![](TM5_9_3Errors11hours.png) | ... | ... |
+| 10 | ![](Zen_10_Setups55_0Errors.png) | ![](TM5_10_NoError18hours.png) ![](TM5_10_part2Errorsafter35hours.png) | ... | ... |
+| 11 | ![](Zen_11_0_ErrorsSoFar.png) ![](Zen_11_0_ErrorsSoFar2.png) | ![](TM5_11_0ErrorsSoFar.png) | ... | ... |
+
+## More blah
+
+Blahblahblah
